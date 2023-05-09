c323130ee5f741c360e2e4e4ade5ca888f9eac46
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2023-05-09 18:41:36 -0400

    first attempt at building raven-hydro using scikit-build-core and pybind11

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <pybind11/pybind11.h>
+
+#define STRINGIFY(x) #x
+#define MACRO_STRINGIFY(x) STRINGIFY(x)
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(_core, m) {
+    m.doc() = R"pbdoc(
+        Pybind11 example plugin
+        -----------------------
+
+        .. currentmodule:: raven
+
+        .. autosummary::
+           :toctree: _generate
+
+           square
+    )pbdoc");
+
+#ifdef VERSION_INFO
+    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+#else
+    m.attr("__version__") = "dev";
+#endif
+}
